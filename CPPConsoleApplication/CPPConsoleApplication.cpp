7102817990997166f573a79226acab5ee76a3268
Jeff Brown <39058795+daddyjab@users.noreply.github.com>
2019-10-11 15:13:44 -0500

    Preparing to switch to function vs. inline approach

--- CPPConsoleApplication/CPPConsoleApplication.cpp
+++ CPPConsoleApplication/CPPConsoleApplication.cpp
@@ -5,9 +5,41 @@
 // #include <string>  -- no need to include <string> explicitly, since already in <iostream> library
 using namespace std;
 
+
+int output_arr_info(int* a_arr, int a_len) {
+
+	// Start the line containing the list of array elements
+	cout << "{ ";
+
+	// Initial the array sum to 0
+	int x_sum = 0;
+
+	// Loop through all elements in the array arr
+	for (int i = 0; i < a_len; i++) {
+
+		// If this is not the first element, add a separator
+		if (i > 0) cout << ", ";
+
+		// Output the value of the array element
+		cout << a_arr[i];
+
+		// Accumulate the sum
+		x_sum += a_arr[i];
+	}
+
+	// End the line containing the list of array elements
+	cout << " }" << endl;
+
+	// Display the sum
+	cout << "Sum of array elements: " << x_sum << endl;
+
+	return x_sum;
+}
+
+
 int main()
 {
-	// An array of integers
+	// An array of integers - static allocation
 	int arr[] = { 11, 22, 33, 44, 55 };
 
 	// Output a start up message
@@ -17,7 +49,7 @@ int main()
 	int arr_len = size(arr);
 
 	// Display the list of all array elements on the same line, then the sum
-	cout << "Array [ " << arr_len << " elements ]: {";
+	cout << "Statically Allocated Array [ " << arr_len << " elements ]: {";
 
 	// Initial the array sum to 0
 	int arr_sum = 0;
@@ -38,8 +70,63 @@ int main()
 	// End the line containing the list of array elements
 	cout << "}" << endl;
 
-		// Display the sum
-		cout << "Sum of array elements: " << arr_sum << endl;
+	// Display the sum
+	cout << "Sum of array elements: " << arr_sum << endl;
+
+	// Try the function
+	cout << "Function does this:" << endl;
+	int f_sum = output_arr_info(arr, 5);
+
+
+
+	/*********************************************************************************************************/
+
+	// An array of integers - dynamically allocated
+	int *arr_ptr = NULL;
+	arr_ptr = new int[5];
+
+	// Size of the dynamically allocated array
+	int arr_ptr_len = 5;
+	//int arr_ptr_len = sizeof(*arr_ptr) / sizeof(arr_ptr[0]);
+	//cout << "sizeof(*arr_ptr) [" << sizeof(*arr_ptr) << "]" << endl;
+	//cout << "sizeof(arr_ptr[0]) [" << sizeof(arr_ptr[0]) << "]" << endl;
+
+	// Initialize the dynamically allocated array
+	for (int j = 0; j < arr_ptr_len; j++)
+		arr_ptr[j] = 111 * (j + 1);
+
+	// Display the list of all array elements on the same line, then the sum
+	cout << "Dynamically Allocated Array [ " << arr_ptr_len << " elements ]: ";
+
+	// Initial the array sum to 0
+	int arr_ptr_sum = 0;
+
+	// Loop through all elements in the array arr
+	for (int i = 0; i < arr_ptr_len; i++) {
+
+		// If this is not the first element, add a separator
+		if (i > 0) cout << ", ";
+
+		// Output the value of the array element
+		cout << arr_ptr[i];
+
+		// Accumulate the sum
+		arr_ptr_sum += arr_ptr[i];
+	}
+
+	// End the line containing the list of array elements
+	cout << "}" << endl;
+
+	// Display the sum
+	cout << "Sum of array elements: " << arr_ptr_sum << endl;
+
+	// Try the function
+	cout << "Function does this:" << endl;
+	int f_sum_ptr = output_arr_info(arr_ptr, 5);
+
+	// Delete the dynamically allocated array
+	delete [] arr_ptr;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
